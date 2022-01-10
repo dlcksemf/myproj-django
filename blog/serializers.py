b217f6f25d83afd587fb0dcdda0f84d0f326cc61
Chandeul Lee <22chandl@gmail.com>
2022-01-10 14:19:57 +0900

    DRF를 활용하여 Post REST API 구현

---
+++ blog/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from blog.models import Post
+
+class PostSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__" # 실서비스에서는 비추.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from blog.serializers import PostSerializers
+from blog.models import Post
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
