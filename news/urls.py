07e54c2147092334ed0e7b8589e6a2defb4c4e01
Chandeul Lee <22chandl@gmail.com>
2022-01-12 10:25:03 +0900

    Article 모델 POST API 생성

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from news.views import ArticleViewSet
 
 app_name = "news"
 
-urlpatterns = [
+router = DefaultRouter()
+router.register("articles", ArticleViewSet)
 
+urlpatterns = [
+    path("api/", include(router.urls)),
 ]
